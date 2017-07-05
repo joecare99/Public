8ae9d4582914cead7fbd65141a5ae8f7b7a7b1bb
Joe Care <Joe_care_99@yahoo.com>
2017-07-05 23:25:31 +0200

    MDI-Test

--- Examples/Source/MDIPretest/frm_MDIPretestMain.pas
+++ Examples/Source/MDIPretest/frm_MDIPretestMain.pas
@@ -6,7 +6,7 @@ interface
 
 uses
   Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs, ComCtrls,
-  Menus, ActnList, ExtCtrls, formpanel, titlebar, buttonsbar;
+  Menus, ActnList, ExtCtrls, Buttons, StdCtrls, formpanel, titlebar, buttonsbar;
 
 type
 
@@ -14,14 +14,20 @@ type
 
   TForm1 = class(TForm)
     ActionList1: TActionList;
+    BitBtn1: TBitBtn;
     FormPanel1: TFormPanel;
     FormPanel2: TFormPanel;
+    LabeledEdit1: TLabeledEdit;
     MainMenu1: TMainMenu;
+    Memo1: TMemo;
     Panel1: TPanel;
     ScrollBox1: TScrollBox;
     StatusBar1: TStatusBar;
+    StatusBar2: TStatusBar;
     TabControl1: TTabControl;
     ToolBar1: TToolBar;
+    procedure FormPanel1Click(Sender: TObject);
+    procedure FormPanel2Click(Sender: TObject);
   private
 
   public
@@ -34,6 +40,18 @@ var
 implementation
 
 {$R *.lfm}
+
+{ TForm1 }
+
+procedure TForm1.FormPanel1Click(Sender: TObject);
+begin
+  FormPanel1.BringToFront;
+end;
+
+procedure TForm1.FormPanel2Click(Sender: TObject);
+begin
+  Formpanel2.BringToFront;
+end;
 
 end.
 
