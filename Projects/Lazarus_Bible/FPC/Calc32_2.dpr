87a9b1fa943ba4e43ff346cbfa5f6d7db7da5a80
Joe Care <Joe_care_99@yahoo.com>
2019-02-19 23:46:06 +0100

    Modified Calc32_2

--- Projects/Lazarus_Bible/FPC/Calc32_2.dpr
+++ Projects/Lazarus_Bible/FPC/Calc32_2.dpr
@@ -2,6 +2,8 @@ program Calc32_2;
 
 {$IFDEF FPC}
   {$MODE Delphi}
+{$ELSE}
+  {$E EXE}
 {$ENDIF}
 
 uses
@@ -12,13 +14,12 @@ uses
   Forms,
   Frm_CALC2 in '..\source\CALC32\Frm_CALC2.PAS' {CalcForm},
   Frm_Calc2ABOUT in '..\source\CALC32\Frm_Calc2ABOUT.PAS' {AboutForm};
-{$E EXE}
 
 begin
   Application.Initialize;
   Application.Title := 'Taschenrechner';
-  Application.CreateForm(TCalcForm, CalcForm);
-  Application.CreateForm(TAboutForm, AboutForm);
+  Application.CreateForm(TfrmCalcMain2, frmCalcMain2);
+  Application.CreateForm(TfrmCalcAbout2, frmCalcAbout2);
   Application.Run;
 end.
 
