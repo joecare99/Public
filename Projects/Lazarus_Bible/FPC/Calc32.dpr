b66db9d7227c7bef6c9fc59d82cef86615a3873d
Joe Care <Joe_care_99@yahoo.com>
2019-02-21 01:05:25 +0100

    Some Improvements in Calc32

--- Projects/Lazarus_Bible/FPC/Calc32.dpr
+++ Projects/Lazarus_Bible/FPC/Calc32.dpr
@@ -9,7 +9,7 @@ uses
 {$ELSE}
   Interfaces,
 {$ENDIF}
-  Forms,
+  Forms, SysUtils,
   Frm_CALC in '..\source\CALC32\Frm_CALC.PAS' {CalcForm},
   Frm_CalcABOUT in '..\source\CALC32\Frm_CalcABOUT.PAS' {AboutForm};
 {$IFNDEF FPC}
@@ -17,10 +17,22 @@ uses
 {$ENDIF}
 
 {$R *.res}
+function AppVendorName: String;
+begin
+  Result := 'JC-Soft'
+end;
+
+function AppApplicationName: String;
+begin
+  Result := 'Calc32';
+end;
 
 begin
-Application.Initialize;
+  Application.Scaled:=True;
+  Application.Initialize;
   Application.Title := 'Demo: Calc32';
+  OnGetVendorName:=AppVendorName;
+  OnGetApplicationName:=AppApplicationName;
   Application.CreateForm(TfrmCalcMain, frmCalcMain);
   Application.CreateForm(TfrmCalcAbout, frmCalcAbout);
   Application.Run;
