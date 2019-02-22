b04da1c7d8fecf2d1064bbc1886b0511faa0cd62
Joe Care <Joe_care_99@yahoo.com>
2019-02-22 19:54:41 +0100

    Project Files for D2007

---
+++ Projects/Lazarus_Bible/source/IPC/2007/Server2.dpr
@@ -0,0 +1,15 @@
+program Server2;
+
+uses
+  Forms,
+  Frm_SERVER2 in '..\Frm_SERVER2.PAS' {ServerForm};
+
+{$R *.RES}
+{$E EXE}
+
+begin
+Application.Initialize;
+  Application.Title := 'Demo: Server2';
+  Application.CreateForm(TServerForm, ServerForm);
+  Application.Run;
+end.
