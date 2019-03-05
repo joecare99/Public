49de6370f2104224b0fb3ef82ab9de44cfe8a9c2
Joe Care <Joe_care_99@yahoo.com>
2019-03-05 18:30:15 +0100

    Diverses/IUPCC: New examples

--- Diverses/Source/iupcc/unt_iupcc_bsp.pas
+++ Diverses/Source/iupcc/unt_iupcc_bsp.pas
@@ -10,7 +10,10 @@ uses
 const
     CntCnt = 10;
     CntData: array[1..CntCnt - 1] of single = (1,2,3,4,5,6,7,8.9,10);
+    
+    Test = 'ABCDE' {Werte von für SQR((A * A}+{B * B)) mit #23 bei 'A', 'B' bei #4 ( und zusätzlich }'F´ bei ´H' ;
 
+    
 implementation
 
 
@@ -19,6 +22,7 @@ function hash(Value:integer):integer;
 begin
   result := abs(Value) mod 23;
 end;
+
 
 end.
 
