2280beebc1f98317776f2d5b7cadd43d6291d5af
Joe Care <Joe_care_99@yahoo.com>
2019-03-19 00:36:53 +0100

    Games/Kingdom: Engine and Texts

--- Games/Source/Kingdom/cls_KingdomEng.pas
+++ Games/Source/Kingdom/cls_KingdomEng.pas
@@ -1,6 +1,8 @@
 unit cls_KingdomEng;
 
+{$IFDEF FPC}
 {$mode objfpc}{$H+}
+{$ENDIF}
 
 interface
 
@@ -13,85 +15,189 @@ type
 
   TKingdomEngine=Class
      private
-       P, // Population
-         S, // Storage
-         H, // Harvested
-         E, // Ratts
-         Y, // Productivity
-         A, // Area
-         I, // Immigrants
-         D, // Death
-         Z, // YearOfReigh
-         Prd, // ProductionLand
-         CoL, // CostOfLand
-         Q:integer;
+       FPlague: Boolean; // A Plague happend this year
+       FExpelled: Boolean; // You were expelled from the Land
+       FYearOfReighn, // YearOfReigh
+       FPopulation, // Population
+       FStorage, // Storage
+       FHarvest, // Harvested
+       FArea:integer; // Area
+
+         // Decitions
+         FLandInProduction, // ProductionLand
+         FDistributedFood:integer;  //Distributed Food
+
+         // Results
+         FRatts, // Ratts
+         FImmigrants, // Immigrants
+         FDeath, // Death
+       FCostOfLand, // CostOfLand
+       FProductivity:Integer; // Productivity
+         //Score
+        FPopSum,
+        FDeathSum:integer;
+        function GetGameEnded: Boolean;
       public
         function BuySellLand(aValue:integer):boolean;
         function Distribute(aValue:integer):boolean;
         function Production(aValue:integer):boolean;
-        PRocedure NewYear;
-         Property LandPrice:integer read CoL ;
-         PRoperty Area:integer read A;
-         property Population:integer read P;
-         Property Storage:integer read S;
-
+        function GameDescription:String;
+        function YearDescription:String;
+        function BuySellMsg(aValue:integer):String;
+        function DistrMsg(aValue:integer):String;
+        function ProdMsg(aValue:integer):String;
+        Procedure NewGame;
+        procedure NewYear(keep: boolean);
+         Property LandPrice:integer read FCostOfLand ;
+         PRoperty Area:integer read FArea;
+         property Population:integer read FPopulation;
+         Property Storage:integer read FStorage;
+         property LandInProd:integer read FLandInProduction;
+         Property Distributed:integer read FDistributedFood;
+         Property GameEnded:Boolean read GetGameEnded;
 
   end;
 
 implementation
 
+uses unt_KingdomBase;
+
 { TKingdomEngine }
+
+function TKingdomEngine.GetGameEnded: Boolean;
+begin
+  result := (FYearOfReighn > 10) or FExpelled ;
+end;
 
 function TKingdomEngine.BuySellLand(aValue: integer): boolean;
 begin
-  if aValue * CoL > S then
+  if aValue * FCostOfLand > (FStorage - FLandInProduction div 2) then
     exit(false)
-  else if -aValue > A then
+  else if -aValue > FArea then
     exit(false)
   else
     begin
-      A:=a+aValue;
-      S:= S-aValue*CoL;
+      FArea:=FArea+aValue;
+      FStorage:= FStorage-aValue*FCostOfLand;
       result := true;
     end;
 end;
 
 function TKingdomEngine.Distribute(aValue: integer): boolean;
 begin
-  if aValue<0 or aValue>S then exit(false);
-  Q:=Q+aValue;
-  S:= S-aValue;
+  if (aValue<0) or (aValue>FStorage - FLandInProduction div 2) then exit(false);
+  FDistributedFood:=FDistributedFood+aValue;
+  FStorage:= FStorage-aValue;
+  result:=true;
 end;
 
 function TKingdomEngine.Production(aValue: integer): boolean;
 begin
-  if (aValue>A) or (aValue<1) or (aValue>S*2) or (aValue > 10*P) then exit(false)
-  PRd:=Avalue;
+  if (aValue>FArea) or (aValue<1) or (aValue>FStorage*2) or (aValue > 10*FPopulation) then
+      exit(false);
+  FLandInProduction:=Avalue;
+  result:=true;
 end;
 
-procedure TKingdomEngine.NewYear;
+function TKingdomEngine.GameDescription: String;
+begin
+  result := rsGameDescription;
+end;
+
+function TKingdomEngine.YearDescription: String;
+begin
+  if not FExpelled then
+  result:=rsAdress +LineEnding+
+    format(rsYearOfReign, [Numbers[FYearOfReighn],FDeath,FImmigrants]);
+  // Todo: more Year Desription
+
+end;
+
+function TKingdomEngine.BuySellMsg(aValue: integer): String;
+begin
+  result:='Geht nicht ...'; // Todo: no BuySell-Message
+end;
+
+function TKingdomEngine.DistrMsg(aValue: integer): String;
+begin
+  result:='Geht nicht ...'; // Todo: no Distr-Message
+end;
+
+function TKingdomEngine.ProdMsg(aValue: integer): String;
+begin
+  result:='Geht nicht ...'; // Todo: no Prod-Message
+end;
+
+procedure TKingdomEngine.NewGame;
+begin
+  FPopulation:=100;
+  FStorage:=2800;
+  FArea:=1000;
+  FProductivity:=3;
+  FLandInProduction:=1000;
+  FHarvest:=3000;
+  FRatts:=200;
+  FImmigrants:=5;
+  FDeath:=0;
+  FYearOfReighn:=1;
+  FPlague:=false;
+  FExpelled:=false;
+  FCostOfLand:=random(10)+16;
+end;
+
+procedure TKingdomEngine.NewYear(keep:boolean);
 var
   lv: Integer;
+  lFullySupportedPeop:integer;
 begin
-  Z := Z + 1;
+  if (FYearOfReighn>10) or FExpelled then exit;
   // Produktionskosten
-  S := S- Prd div 2;
+  FStorage := FStorage- FLandInProduction div 2;
   // Ertrag pro LAnd
-  Y := random(5)+1;
+  FProductivity := random(5)+1;
   // Ernte
-  H := Prd * Y;
+  FHarvest := FLandInProduction * FProductivity;
   // Verlust durch Ratten
   lv := random(5)+1;
-  if lv mod 2=0 then E:= S div lv;
+  if lv mod 2=0 then FRatts:= FStorage div lv;
   // Restspeicher
-  S := S - E + H;
+  FStorage := FStorage - FRatts + FHarvest;
   // Immigranten
   lv:= Random(5)+1;
-  I := lv * (20*a+S) div (P * 100) +1;
-  // Todesfälle
-  C = Q div 20;
-  lv := trunc(10* (2*Random -0.3));
+  FImmigrants := lv * (20*FArea+FStorage) div (FPopulation * 100) +1;
+  // Ist dieses Jahr eine Seuche aufgetreten ?
+  FPlague:=random(20)=0;
 
+  // Todesfälle durch Hunger
+  // unter der Annahme, daß Geburten und altersbedingte Todesfälle die Waage halten
+  lFullySupportedPeop  := (FDistributedFood div 20);
+
+  lv := trunc(10* (2*Random -0.3));
+  if lFullySupportedPeop > FPopulation then
+    FDeath:=0
+  else
+    begin
+      FDeath := FPopulation-lFullySupportedPeop;
+      FPopulation:=lFullySupportedPeop;
+      FExpelled:=(FDeath*2)>FPopulation;
+    end;
+
+  FPopSum:=((FYearOfReighn-1)* FPopSum+(FDeath*100) div FPopulation) div FYearOfReighn;
+  FDeathSum := FDeathSum+FDeath;
+
+  FPopulation:=FPopulation + FImmigrants;
+  if FPlague then
+    FPopulation:=FPopulation div 2;
+
+  //Reset Values
+  if not Keep then
+    begin
+      FLandInProduction:=0;
+      FDistributedFood:=0;
+    end;
+  // Werte für nächstes Jahr
+  FYearOfReighn := FYearOfReighn + 1;
+  FCostOfLand:=random(10)+16;
 end;
 
 
---
+++ Games/Source/Kingdom/unt_KingdomBase.pas
@@ -0,0 +1,46 @@
+unit unt_KingdomBase;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils;
+
+resourcestring
+// Texte
+   rsGameDescription = 'Dies ist das Königreich Sumeria, und Sie wurden für 10 '+
+     'Jahre zum Herscher gewählt.'+LineEnding+LineEnding+
+     'Ihre Herrschaft entscheidet über Leben und Tod von hunderten ihrer '+
+     'Untertanen. Ihre diktatorischen Fähigkeiten werden am Ende der Ampsperiode'+
+     ', nach 10 Jahren geprüft.'+LineEnding+LineEnding+
+     'Jedes Jahr müssen Sie Ihre Entscheidungen überdenken und ausführen.';
+   rsAdress='Hamurabi, ich habe Dir folgendes zu berichten:';
+   rsYearOfReign='Dies ist das %s Jahr deiner Regentschaft sind %d Leute '+
+     'gestorben; %d Leute sind nach Sumeria zugezogen.';
+   rsPlague='Eine schreckliche Plage hat dein Land heimgesucht und die Hälfte '+
+     'der Bevölkerung dahingerafft...';
+   rsOK='Erledigt ...';
+
+
+// Zahlen
+   rsFirst='erste';
+   rsSecond='zweite';
+   rsThird='dritte';
+   rsFourth='vierte';
+   rsFifth='fünfte';
+   rsSixth='sechste';
+   rsSeventh='siebente';
+   rsEighth='achte';
+   rsNineth='neunte';
+   rsTenth='zehnte';
+   rsEleventh='elfte';
+
+
+const Numbers:array[1..11] of string=
+  (rsFirst,rsSecond,rsThird,rsFourth,rsFifth,rsSixth,rsSeventh,rsEighth,rsNineth,rsTenth,rsEleventh);
+
+implementation
+
+end.
+
