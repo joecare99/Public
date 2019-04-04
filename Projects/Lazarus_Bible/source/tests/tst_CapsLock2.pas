f379a5929bfb572f827243e3ca36db75ddebf8e4
Joe Care <Joe_care_99@yahoo.com>
2019-04-05 01:46:12 +0200

    Lazarus_Bible/CapsLock2: Unit-Test

--- Projects/Lazarus_Bible/source/tests/tst_CapsLock2.pas
+++ Projects/Lazarus_Bible/source/tests/tst_CapsLock2.pas
@@ -11,29 +11,65 @@ uses
 
 type
 
+  { TTestCapsLock2 }
+
   TTestCapsLock2= class(TTestCase)
+  private
+    FIdleCnt: Integer;
+    procedure AppUserInput(Sender: TObject; Msg: Cardinal);
   protected
     procedure SetUp; override;
     procedure TearDown; override;
   published
-    procedure TestHookUp;
+    procedure TestSetUp;
+     Procedure TestMainForm;
   end;
 
 implementation
 
-procedure TTestCapsLock2.TestHookUp;
+uses Forms, Frm_Capslock2MAIN;
+
+procedure TTestCapsLock2.TestSetUp;
 begin
-  Fail('Write your own test');
+  CheckNotNull(frmCapsLock2Main,'CapsLock2Main ist assigned');
+  CheckTrue(frmCapsLock2Main.Visible);
+end;
+
+procedure TTestCapsLock2.TestMainForm;
+var
+  lOrgCaption: String;
+begin
+  lOrgCaption:= frmCapsLock2Main.Caption;
+  Application.OnUserInput:=@AppUserInput;
+  while frmCapsLock2Main.Visible do
+  begin
+        Application.Idle(false);
+        Application.ProcessMessages;
+        inc(fIdleCnt);
+        sleep(10);
+        if fIdleCnt> 300 then
+          frmCapsLock2Main.Hide
+        else
+          frmCapsLock2Main.Caption := 'CapsLock ['+StringOfChar('|',30-fIdleCnt div 10)+StringOfChar(' ',fIdleCnt div 10)+']';
+     end;
+  frmCapsLock2Main.Caption:=lOrgCaption;
+end;
+
+procedure TTestCapsLock2.AppUserInput(Sender: TObject; Msg: Cardinal);
+begin
+  FIdleCnt:=0;
 end;
 
 procedure TTestCapsLock2.SetUp;
 begin
-
+  if not assigned(frmCapsLock2Main) then
+    Application.CreateForm(TfrmCapsLock2Main,frmCapsLock2Main);
+  frmCapsLock2Main.show;
 end;
 
 procedure TTestCapsLock2.TearDown;
 begin
-
+   frmCapsLock2Main.hide;
 end;
 
 initialization
