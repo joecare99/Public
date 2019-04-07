00e9630cf28a3249ce14b556a1838f4d861d5182
Joe Care <Joe_care_99@yahoo.com>
2019-04-07 22:25:23 +0200

    Grafik\RenderImage Unit-Test (vMul)

--- Grafik/Source/RenderImage/tst_RenderBase.pas
+++ Grafik/Source/RenderImage/tst_RenderBase.pas
@@ -286,8 +286,33 @@ begin
 end;
 
 procedure TTestRenderBase.TestVMul;
+var
+  x1, y1,x2,y2: Extended;
+  i: Integer;
+  lFTupple:TFTuple;
 begin
-
+ CheckEquals(FTuple(0,0),ZeroTup.VMul(ZeroTup),1e-20,'ZeroTup - ZeroTup');
+ FFtupple.init(0,0);
+ CheckEquals(FTuple(0,0),FFtupple.VMul(ZeroTup),1e-20,'init(0,0) - ZeroTup');
+ FFtupple.init(1.0,-1.0);
+ CheckEquals(FTuple(0.0,-4.0),FFtupple.VMul(FTuple(2.0,-2.0)),1e-20,'init(1.0,-1.0).VMul(<2,-2>)');
+ CheckEquals(1.0,FFtupple.v[0],format('init(%f,%f).v[0]',[1.0,-1.0]));
+ CheckEquals(-1.0,FFtupple.v[1],format('init(%f,%f).v[1]',[1.0,-1.0]));
+ FFtupple.init(23.0,17.0);
+ CheckEquals(FTuple(120.0,-18.0),FFtupple.VMul(FTuple(3.0,-3.0)),1e-20,'init(23.0,17.0).VMul(<-3,3)');
+ CheckEquals(120.0,FFtupple.VMul(FTuple(3.0,-3.0)).v[0],format('init(%f,%f).v[0]',[23.0,17.0]));
+ CheckEquals(-18.0,FFtupple.VMul(FTuple(3.0,-3.0)).v[1],format('init(%f,%f).v[1]',[23.0,17.0]));
+ for i := 0 to 50000 do
+   begin
+     x1:= (random-0.5)*maxLongint;
+     y1:= (random-0.5)*maxLongint;
+     x2:= (random-0.5)*maxLongint;
+     y2:= (random-0.5)*maxLongint;
+     FFtupple.init(x1,y1);
+     lFTupple.INit(x2,y2);
+     CheckEquals(FTuple(x1*x2-y1*y2,x2*y1+y2*x1),FFtupple.VMul(lFTupple),1e-20,format('init(%f,%f).VMul(%f,%f)',[x1,y1,x2,y2]));
+     CheckEquals(FTuple(x1,y1),FFtupple,1e-20,format('FTupple=(%f,%f)',[x1,y1]));
+   end;
 end;
 
 procedure TTestRenderBase.TestEquals;
