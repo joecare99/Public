4a79afde22125877bdc936638f3f3fdbc76c758d
Joe Care <Joe_care_99@yahoo.com>
2019-04-10 13:33:51 +0200

    Grafik\RenderImage Basic Thoughts

--- Grafik/Source/RenderImage/cls_RenderEngine.pas
+++ Grafik/Source/RenderImage/cls_RenderEngine.pas
@@ -5,13 +5,40 @@ unit cls_RenderEngine;
 interface
 
 uses
-  Classes, SysUtils, ;
+  Classes, SysUtils, cls_RenderBase;
 
-type TRenderEngine=Class
-    FObjects:array of T
+type
+
+{ TRenderEngine }
+
+ TRenderEngine=Class
+    FObjects:array of TRenderBaseObject;
+    FCamera:TRenderCamera;
+    FResult:TBitmap;
+    Procedure Render;
   end;
 
 implementation
+
+{ TRenderEngine }
+
+procedure TRenderEngine.Render;
+begin
+  // Parse
+  // Optimize
+  // Split into Tasks
+  // Per Task do:
+  //  - Select a Ray
+  //  - Follow the ray until it hits an Object.
+  //  - Calculete The Hitpoint of the Object
+  //  - Calculate the Light&Color of the Hitpoint by:
+  //  - - SelfLightness
+  //  - - Calculate the Light-Rays from the Hitpoint to the Light-Sources
+  //  - - In case of reflection Split the Ray into a new Ray from the Hitpoint according to the Reflection-Propety of the Hitpoint.
+  //  - - In Case of Refraction Split the Ray into a New Ray from the Hitpoint according to the Refraction-Property.
+  //  - - Both split rays are Followed until a maximum of splits is reached or the relevance is canceled by a bail-out-Value.
+  //  - All three Rays add up to the Color of the Original Ray.
+end;
 
 end.
 
