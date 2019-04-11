33ee55494befcfa29f16e41ffc5d5b30060a3852
Joe Care <Joe_care_99@yahoo.com>
2019-04-11 08:58:45 +0200

    Grafik\RenderImage LightSources & Trace (Stumb-Only)

--- Grafik/Source/RenderImage/cls_RenderEngine.pas
+++ Grafik/Source/RenderImage/cls_RenderEngine.pas
@@ -5,7 +5,7 @@ unit cls_RenderEngine;
 interface
 
 uses
-  Classes, SysUtils, cls_RenderBase;
+  Classes, SysUtils, cls_RenderBase,graphics;
 
 type
 
@@ -13,8 +13,10 @@ type
 
  TRenderEngine=Class
     FObjects:array of TRenderBaseObject;
+    FLightSources:array of TRenderBaseObject;
     FCamera:TRenderCamera;
     FResult:TBitmap;
+    Function Trace(Ray:TRenderRay;Share:extended;Death:integer):TColor;
     Procedure Render;
   end;
 
@@ -22,22 +24,35 @@ implementation
 
 { TRenderEngine }
 
+function TRenderEngine.Trace(Ray: TRenderRay; Share: extended; Death: integer
+  ): TColor;
+begin
+  //  - Follow the ray until it hits an Object.
+
+  //  - Calculete The Hitpoint of the Object
+
+  //  - Calculate the Light&Color of the Hitpoint by:
+  //  - - SelfLightness
+
+  //  - - Calculate the Light-Rays from the Hitpoint to the Light-Sources
+
+  //  - - In case of reflection Split the Ray into a new Ray from the Hitpoint according to the Reflection-Propety of the Hitpoint.
+
+  //  - - In Case of Refraction Split the Ray into a New Ray from the Hitpoint according to the Refraction-Property.
+  //  - - Both split rays are Followed until a maximum of splits is reached or the relevance is canceled by a bail-out-Value.
+  //  - All three Rays add up to the Color of the Original Ray.
+end;
+
 procedure TRenderEngine.Render;
+
+var Ray:TRenderRay;
 begin
   // Parse
   // Optimize
   // Split into Tasks
   // Per Task do:
   //  - Select a Ray
-  //  - Follow the ray until it hits an Object.
-  //  - Calculete The Hitpoint of the Object
-  //  - Calculate the Light&Color of the Hitpoint by:
-  //  - - SelfLightness
-  //  - - Calculate the Light-Rays from the Hitpoint to the Light-Sources
-  //  - - In case of reflection Split the Ray into a new Ray from the Hitpoint according to the Reflection-Propety of the Hitpoint.
-  //  - - In Case of Refraction Split the Ray into a New Ray from the Hitpoint according to the Refraction-Property.
-  //  - - Both split rays are Followed until a maximum of splits is reached or the relevance is canceled by a bail-out-Value.
-  //  - All three Rays add up to the Color of the Original Ray.
+  //call Trace(Ray, 1.0, 1)
 end;
 
 end.
---
+++ Grafik/Source/RenderImage/cls_RenderSimpleObjects.pas
@@ -0,0 +1,13 @@
+unit cls_RenderSimpleObjects;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, cls_RenderBase;
+
+implementation
+
+end.
+
