be7bfc8607d5b05364b7c1bee69171c80c670475
Joe Care <Joe_care_99@yahoo.com>
2020-09-12 00:48:48 +0200

    Laby #4

--- Diverses/Source/frm_laby4.pas
+++ Diverses/Source/frm_laby4.pas
@@ -1,125 +1,134 @@
-Unit frm_laby4;
+unit frm_laby4;
 
 {$IFDEF FPC}
   {$MODE Delphi}
 {$ENDIF}
 
-Interface
+interface
 
-Uses
+uses
 {$IFnDEF FPC}
-  Windows,
+    Windows,
 {$ELSE}
-  LCLIntf, LCLType,
+    LCLIntf, LCLType,
 {$ENDIF}
-  SysUtils, Variants, Classes, Graphics, Controls, Forms,
-  Dialogs, ExtCtrls,  Buttons, StdCtrls;
+    SysUtils, Variants, Classes, Graphics, Controls, Forms,
+    Dialogs, ExtCtrls, Buttons, StdCtrls;
 
-Type
-  TForm2 = Class(TForm)
-    BitBtn1: TBitBtn;
-    PaintBox1: TPaintBox;
-    PaintBox2: TPaintBox;
-    BitBtn2: TBitBtn;
-    Procedure BitBtn1Click(Sender: TObject);
-    Procedure BitBtn2Click(Sender: TObject);
-  Private
-    Clen: integer;
-    FZValues: Array Of Array Of integer;
-    { Private-Deklarationen }
-  Public
-    { Public-Deklarationen }
-  End;
+type
 
-Var
-  Form2: TForm2;
+    { TForm2 }
 
-Implementation
+    TForm2 = class(TForm)
+      BitBtn1: TBitBtn;
+      BitBtn2: TBitBtn;
+        PaintBox1: TPaintBox;
+        PaintBox2: TPaintBox;
+        Panel1: TPanel;
+        procedure BitBtn1Click(Sender: TObject);
+        procedure BitBtn2Click(Sender: TObject);
+    private
+        FZValues: array of array of integer;
+        { Private-Deklarationen }
+    public
+        { Public-Deklarationen }
+    end;
+
+var
+    Form2: TForm2;
+
+
+const     Clen = 30;
+
+implementation
 
 {$IFnDEF FPC}
   {$R *.dfm}
+
 {$ELSE}
   {$R *.lfm}
 {$ENDIF}
 
-Procedure TForm2.BitBtn1Click(Sender: TObject);
-  Var
+procedure TForm2.BitBtn1Click(Sender: TObject);
+var
     x, y, rr: integer;
-  Begin
-    Clen := 20;
+begin
     rr := random(3) + 2;
     Randomize;
     setlength(FZValues, Clen, Clen);
     // rohdaten
-    For x := 0 To Clen - 1 Do
-      For y := 0 To Clen - 1 Do
-        Begin
-          FZValues[x, y] := x + y + random(rr);
-          If (x >= 1) And (FZValues[x, y] < FZValues[x - 1, y] - 1) Then
-            FZValues[x, y] := FZValues[x - 1, y] - 1;
-          If (y >= 1) And (FZValues[x, y] < FZValues[x, y - 1] - 1) Then
-            FZValues[x, y] := FZValues[x, y - 1];
-          If (y >= 1) And (x >= 1) And
-            (FZValues[x, y] < FZValues[x - 1, y - 1] - 1) Then
-            FZValues[x, y] := FZValues[x - 1, y - 1] - 1;
-        End;
+    for x := 0 to Clen - 1 do
+        for y := 0 to Clen - 1 do
+          begin
+            FZValues[x, y] := x + y + random(rr);
+            if (x >= 1) and (FZValues[x, y] < FZValues[x - 1, y] - 1) then
+                FZValues[x, y] := FZValues[x - 1, y] - 1;
+            if (y >= 1) and (FZValues[x, y] < FZValues[x, y - 1] - 1) then
+                FZValues[x, y] := FZValues[x, y - 1];
+            if (y >= 1) and (x >= 1) and (FZValues[x, y] <
+                FZValues[x - 1, y - 1] - 1) then
+                FZValues[x, y] := FZValues[x - 1, y - 1] - 1;
+          end;
     // nicht verbundene zellen
-    For x := 0 To Clen - 1 Do
-      For y := 0 To Clen - 1 Do
-        Begin
-      //    c := 0;
-          If (x >= 1) And (abs(FZValues[x, y] - FZValues[x - 1, y]) < 2) And
-            (x < Clen - 1) And (abs(FZValues[x, y] - FZValues[x + 1, y]) < 2)
-            And (y >= 1) And (abs(FZValues[x, y] - FZValues[x, y - 1]) < 2) And
-            (y < Clen - 1) And (abs(FZValues[x, y] - FZValues[x, y]) < 2) Then
-            Begin
-              If abs(x - (Clen Div 2)) > abs(y - (Clen Div 2)) Then
-                If x > Clen Div 2 Then
-                  FZValues[x, y] := FZValues[x - 1, y] + 1
-                Else
-                  FZValues[x, y] := FZValues[x + 1, y] - 1
-              Else If y > Clen Div 2 Then
-                FZValues[x, y] := FZValues[x, y - 1] + 1
-              Else
-                FZValues[x, y] := FZValues[x, y + 1] - 1;
+    for x := 0 to Clen - 1 do
+        for y := 0 to Clen - 1 do
+          begin
+            //    c := 0;
+            if (x >= 1) and (abs(FZValues[x, y] - FZValues[x - 1, y]) < 2) and
+                (x < Clen - 1) and (abs(FZValues[x, y] - FZValues[x + 1, y]) <
+                2) and (y >= 1) and (abs(FZValues[x, y] - FZValues[x, y - 1]) < 2) and
+                (y < Clen - 1) and (abs(FZValues[x, y] - FZValues[x, y]) < 2) then
+              begin
+                if abs(x - (Clen div 2)) > abs(y - (Clen div 2)) then
+                    if x > Clen div 2 then
+                        FZValues[x, y] := FZValues[x - 1, y] + 1
+                    else
+                        FZValues[x, y] := FZValues[x + 1, y] - 1
+                else if y > Clen div 2 then
+                    FZValues[x, y] := FZValues[x, y - 1] + 1
+                else
+                    FZValues[x, y] := FZValues[x, y + 1] - 1;
 
-            End;
+              end;
 
-        End;
-    For x := 0 To Clen - 1 Do
-      For y := 0 To Clen - 1 Do
-        Begin
-          PaintBox2.Canvas.Brush.Color :=
-            rgb(FZValues[x, y] * 4, FZValues[x, y] * 4, FZValues[x, y] * 5);
-          PaintBox2.Canvas.FillRect(Rect(x * 10, y * 10, (x + 1) * 10,
-            (y + 1) * 10));
-          If (x >= 1) And (abs(FZValues[x, y] - FZValues[x - 1, y]) < 2) Then
-            Begin
-              PaintBox2.Canvas.pen.Color := clwhite;
-              PaintBox2.Canvas.moveto(x * 10 + 5, y * 10 + 5);
-              PaintBox2.Canvas.Lineto(x * 10 - 5, y * 10 + 5);
+          end;
 
-            End;
-          If (y >= 1) And (abs(FZValues[x, y] - FZValues[x, y - 1]) < 2) Then
-            Begin
-              PaintBox2.Canvas.pen.Color := clwhite;
-              PaintBox2.Canvas.moveto(x * 10 + 5, y * 10 + 5);
-              PaintBox2.Canvas.Lineto(x * 10 + 5, y * 10 - 5);
+    PaintBox2.Width:=CLen*10;
+    PaintBox2.Height:=CLen*10;
+    for x := 0 to Clen - 1 do
+        for y := 0 to Clen - 1 do
+          begin
+            PaintBox2.Canvas.Brush.Color :=
+                rgb(FZValues[x, y] * 3, FZValues[x, y] * 3, FZValues[x, y] * 4);
+            PaintBox2.Canvas.FillRect(Rect(x * 10, y * 10, (x + 1) *
+                10, (y + 1) * 10));
+            if (x >= 1) and (abs(FZValues[x, y] - FZValues[x - 1, y]) < 2) then
+              begin
+                PaintBox2.Canvas.pen.Color := clwhite;
+                PaintBox2.Canvas.moveto(x * 10 + 5, y * 10 + 5);
+                PaintBox2.Canvas.Lineto(x * 10 - 5, y * 10 + 5);
 
-            End;
+              end;
+            if (y >= 1) and (abs(FZValues[x, y] - FZValues[x, y - 1]) < 2) then
+              begin
+                PaintBox2.Canvas.pen.Color := clwhite;
+                PaintBox2.Canvas.moveto(x * 10 + 5, y * 10 + 5);
+                PaintBox2.Canvas.Lineto(x * 10 + 5, y * 10 - 5);
 
-        End;
-  End;
+              end;
 
-Procedure TForm2.BitBtn2Click(Sender: TObject);
+          end;
+end;
 
-    Procedure Drawbox(x, y, z: integer);
-      Var
+procedure TForm2.BitBtn2Click(Sender: TObject);
+
+    procedure Drawbox(x, y, z: integer; DrawHead: boolean);
+    var
         x2, y2, L: integer;
-        pts: Array Of TPoint;
-      Begin
-        L := PaintBox1.Canvas.ClipRect.Right Div 80;
-        x2 := (20 - x + y) * L * 2;
+        pts: array of TPoint;
+    begin
+        L := PaintBox1.Canvas.ClipRect.Right div (Clen*4);
+        x2 := (Clen - x + y) * L * 2;
         y2 := PaintBox1.Canvas.ClipRect.bottom - (x + y + z + 2) * L;
         setlength(pts, 4);
         pts[0] := point(x2, y2);
@@ -138,27 +147,29 @@ Procedure TForm2.BitBtn2Click(Sender: TObject);
         PaintBox1.Canvas.Polygon(pts);
         PaintBox1.Canvas.Brush.Color := clwhite;
         // PaintBox1.Canvas.pen.Color := PaintBox1.Canvas.Brush.Color;
-        pts[1] := point(x2 + L * 2, y2 - L);
-        pts[2] := point(x2, y2 - L * 2);
-        pts[3] := point(x2 - L * 2, y2 - L);
-        PaintBox1.Canvas.Polygon(pts);
-        // PaintBox1.Canvas.PolyLine(pts);
+        if DrawHead then
+          begin
+            pts[1] := point(x2 + L * 2, y2 - L);
+            pts[2] := point(x2, y2 - L * 2);
+            pts[3] := point(x2 - L * 2, y2 - L);
+            PaintBox1.Canvas.Polygon(pts);
+            // PaintBox1.Canvas.PolyLine(pts);
+          end;
+    end;
 
-      End;
-
-  Var
+var
     x, y, z: integer;
 
-  Begin
+begin
     PaintBox1.Canvas.Brush.Color := Color;
     PaintBox1.Canvas.FillRect(PaintBox1.Canvas.ClipRect);
-    For z := 0 To 44 Do
-      For x := Clen - 1 Downto 0 Do
-        For y := Clen - 1 Downto 0 Do
-          If (FZValues[x, y] >= z) And
-            ((x = 0) Or (y = 0) Or (z > x + y - 2)) Then
-            Drawbox(x, y, z);
+    for z := 0 to (Clen*2+4) do
+        for x := Clen - 1 downto 0 do
+            for y := Clen - 1 downto 0 do
+                if (FZValues[x, y] >= z) and ((x = 0) or (y = 0) or
+                    (z > x + y - 2)) then
+                    Drawbox(x, y, z,FZValues[x, y] = z);
 
-  End;
+end;
 
-End.
+end.
