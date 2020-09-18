cf6f04e9178b9b5a421de2557c9f062f58b1356e
Joe Care <Joe_care_99@yahoo.com>
2020-09-18 23:09:23 +0200

    Faster remove draw-commands (better shaddow)

--- Diverses/Source/frm_laby4.pas
+++ Diverses/Source/frm_laby4.pas
@@ -4,6 +4,8 @@ unit frm_laby4;
   {$MODE Delphi}
 {$ENDIF}
 
+{ $define ShowLabyCreation}
+
 interface
 
 uses
@@ -44,7 +46,7 @@ var
 
 
 const
-    Clen = 61;
+    Clen = 21;
 
 implementation
 
@@ -60,13 +62,9 @@ uses unt_Point2d, Printers;
 type
     TPosArray = array[0..3] of TPoint;
 
-const //{-1}prest:String='010011020101111010020201031210000021010020013000111211112010';
-    //{-2}prest:String='1011100100121100000101020001000012131121100020031100011110000000020';
-    //{-2}prest:String='111102010111101001020301102100010001112201210011103101010010211112';
-    {-2}prest: string =
+const
+    prest: string =
         '111102010111101001020301102100010001112201210011103101010010211112';
-    //    102100010001112201
-    //{-3}prest:String='01100020110100010100112100000012131011120100011003001111200101101211';
     sstr: string = '';
 
 function GetRndDir(cnt: integer; PosibDir: TPosArray): TPoint;
@@ -103,31 +101,35 @@ const
     var
         yy, xx: integer;
     begin
-        if mark then
-            PaintBox2.Canvas.Brush.Color :=
-                rgb(63 + (FZValues[x, y] * 192) div (Clen * 2 + 5), 0 +
-                (FZValues[x, y] * 192) div (Clen * 2 + 5), (FZValues[x, y] * 192) div (Clen * 2 + 5))
-        else
-            PaintBox2.Canvas.Brush.Color :=
-                rgb((FZValues[x, y] * 196) div (Clen * 2 + 5), (FZValues[x, y] * 196) div
-                (Clen * 2 + 5), (FZValues[x, y] * 255) div (Clen * 2 + 5));
-        yy := clen - y - 1;
-        xx := clen - x - 1;
-        PaintBox2.Canvas.FillRect(Rect(xx * c * 2, yy * c * 2, (xx + 1) *
-            c * 2, (yy + 1) * c * 2));
-        if (x >= 1) and (abs(FZValues[x, y] - FZValues[x - 1, y]) < 2) then
+        with PaintBox2 do
           begin
-            PaintBox2.Canvas.pen.Color := clwhite;
-            PaintBox2.Canvas.moveto(xx * c * 2 + c, yy * c * 2 + c);
-            PaintBox2.Canvas.Lineto(xx * c * 2 + c * 3, yy * c * 2 + c);
+            if mark then
+                Canvas.Brush.Color :=
+                    rgb(63 + (FZValues[x, y] * 192) div (Clen * 2 + 5),
+                    0 + (FZValues[x, y] * 192) div (Clen * 2 + 5),
+                    (FZValues[x, y] * 192) div (Clen * 2 + 5))
+            else
+                Canvas.Brush.Color :=
+                    rgb((FZValues[x, y] * 196) div (Clen * 2 + 5),
+                    (FZValues[x, y] * 196) div (Clen * 2 + 5),
+                    (FZValues[x, y] * 255) div (Clen * 2 + 5));
+            yy := clen - y - 1;
+            xx := clen - x - 1;
+            Canvas.FillRect(Rect(xx * c * 2, yy * c * 2, (xx + 1) *
+                c * 2, (yy + 1) * c * 2));
+            if (x >= 1) and (abs(FZValues[x, y] - FZValues[x - 1, y]) < 2) then
+              begin
+                Canvas.pen.Color := clwhite;
+                Canvas.moveto(xx * c * 2 + c, yy * c * 2 + c);
+                Canvas.Lineto(xx * c * 2 + c * 3, yy * c * 2 + c);
 
-          end;
-        if (y >= 1) and (abs(FZValues[x, y] - FZValues[x, y - 1]) < 2) then
-          begin
-            PaintBox2.Canvas.pen.Color := clwhite;
-            PaintBox2.Canvas.moveto(xx * c * 2 + c, yy * c * 2 + c);
-            PaintBox2.Canvas.Lineto(xx * c * 2 + c, yy * c * 2 + c * 3);
-
+              end;
+            if (y >= 1) and (abs(FZValues[x, y] - FZValues[x, y - 1]) < 2) then
+              begin
+                Canvas.pen.Color := clwhite;
+                Canvas.moveto(xx * c * 2 + c, yy * c * 2 + c);
+                Canvas.Lineto(xx * c * 2 + c, yy * c * 2 + c * 3);
+              end;
           end;
     end;
 
@@ -151,79 +153,86 @@ begin
     for x := 0 to Clen - 1 do
         for y := 0 to Clen - 1 do
             FZValues[x, y] := 0;
+
     // Laby-Algoritmus
     ActCell := Point2D(2, 0);
     StoredCell := Point2D(ActCell.AsPoint);
-    FZValues[ActCell.x, ActCell.y] :=
-        Baselevel(ActCell.x, ActCell.y);
-    FifoPushIdx := 0;
-    FifoPopIdx := 0;
-    Accu := T2DPoint.Create;
-    SetLength(Fifo{%H-}, Clen * Clen);
-    Dircount := 1;
-    while (DirCount <> 0) or (FifoPushIdx >= FifoPopIdx) do
-      begin
-        // Switch
-        t := ActCell;
-        ActCell := StoredCell;
-        StoredCell := t;
-        // Calculate Valid directions and store them in PosibDir
-        //    PaintField(actcell.x,actcell.y,true);
-        Dircount := 0;
-        for ActDir in dir4 do
-          begin
-            if dircount <= 2 then
-                PosibDir[DirCount + 1] := Point(0, 0);
-            Accu.copy(ActDir).SMult(-2).add(ActCell);
-            if Accu.IsIn(rect(0, 0, Clen, clen)) and (FZValues[Accu.X, Accu.y] = 0) then
-              begin
-                PosibDir[DirCount] := ActDir.asPoint;
-                Inc(Dircount);
-              end;
-          end;
-        if Dircount > 0 then
-          begin
-            ActDir := Accu.Copy(GetRndDir(Dircount, PosibDir));
-          end;
-        if (DirCount > 0) and (not ActDir.Equals(dir4[0])) then
-          begin
-            //      ActDir := GetRndDir(Dircount,PosibDir);  // Calc Destination Cell
-            if dircount > 1 then
-              begin
-                Fifo[FifoPushIdx] := ActCell.AsPoint;
-                Inc(FifoPushIdx);
-              end;
-            ActCell.Subtr(ActDir);
-            FZValues[ActCell.x, ActCell.y] := Baselevel(ActCell.x + 1, ActCell.y);
-            ActCell.Subtr(ActDir);
-            FZValues[ActCell.x, ActCell.y] := Baselevel(ActCell.x, ActCell.y);
-            // Push Cell to the Fifo
-          end
-        else
-          begin
-            // Pop Cell from the Fifo
-            if FifoPushIdx >= FifoPopIdx then
-              begin
-                ActCell.copy(Fifo[FifoPopIdx]);
-                Inc(FifoPopIdx);
-              end;
-          end;
-  (*  if (FifoPushIdx mod Clen =0) {or (length(sstr)>0)} then
-      begin
-    for x := 0 to Clen - 1 do
-         for y := 0 to Clen - 1 do
-           begin
-             PaintField(x,y);
-           end;
-    Application.ProcessMessages;
-    sleep(1);
-       end; *)
-      end;
+    Accu := T2DPoint.init(0, 0);
+      try
 
-    FreeAndNil(accu);
-    FreeAndNil(ActCell);
-    FreeAndNil(StoredCell);
-    SetLength(Fifo, 0);
+        FZValues[ActCell.x, ActCell.y] :=
+            Baselevel(ActCell.x, ActCell.y);
+        FifoPushIdx := 0;
+        FifoPopIdx := 0;
+        SetLength(Fifo{%H-}, Clen * Clen);
+        Dircount := 1;
+        while (DirCount <> 0) or (FifoPushIdx >= FifoPopIdx) do
+          begin
+            // Switch
+            t := ActCell;
+            ActCell := StoredCell;
+            StoredCell := t;
+            // Calculate Valid directions and store them in PosibDir
+            //    PaintField(actcell.x,actcell.y,true);
+            Dircount := 0;
+            for ActDir in dir4 do
+              begin
+                if dircount <= 2 then
+                    PosibDir[DirCount + 1] := Point(0, 0);
+                Accu.copy(ActDir).SMult(-2).add(ActCell);
+                if Accu.IsIn(rect(0, 0, Clen, clen)) and
+                    (FZValues[Accu.X, Accu.y] = 0) then
+                  begin
+                    PosibDir[DirCount] := ActDir.asPoint;
+                    Inc(Dircount);
+                  end;
+              end;
+            if Dircount > 0 then
+              begin
+                ActDir := Accu.Copy(GetRndDir(Dircount, PosibDir));
+              end;
+            if (DirCount > 0) and (not ActDir.Equals(dir4[0])) then
+              begin
+                //      ActDir := GetRndDir(Dircount,PosibDir);  // Calc Destination Cell
+                if dircount > 1 then
+                  begin
+                    Fifo[FifoPushIdx] := ActCell.AsPoint;
+                    Inc(FifoPushIdx);
+                  end;
+                ActCell.Subtr(ActDir);
+                FZValues[ActCell.x, ActCell.y] := Baselevel(ActCell.x + 1, ActCell.y);
+                ActCell.Subtr(ActDir);
+                FZValues[ActCell.x, ActCell.y] := Baselevel(ActCell.x, ActCell.y);
+                // Push Cell to the Fifo
+              end
+            else
+              begin
+                // Pop Cell from the Fifo
+                if FifoPushIdx >= FifoPopIdx then
+                  begin
+                    ActCell.copy(Fifo[FifoPopIdx]);
+                    Inc(FifoPopIdx);
+                  end;
+              end;
+ {$ifdef ShowLabyCreation}
+            if (FifoPushIdx mod Clen = 0) {or (length(sstr)>0)} then
+              begin
+                for x := 0 to Clen - 1 do
+                    for y := 0 to Clen - 1 do
+                      begin
+                        PaintField(x, y);
+                      end;
+                Application.ProcessMessages;
+                sleep(1);
+              end;
+ {$endIf}
+          end;
+      finally
+        FreeAndNil(accu);
+        FreeAndNil(ActCell);
+        FreeAndNil(StoredCell);
+        SetLength(Fifo, 0);
+      end;
 
     // nicht besetzte Zellen
     for x := 0 to Clen - 1 do
@@ -278,40 +287,45 @@ begin
     setlength(pts{%H-}, 4);
     pts[0] := point(x2, y2);
 
-    // PaintBox1.Canvas.pen.Color := PaintBox1.Canvas.Brush.Color;
-    pts[1] := point(x2 + L, y2 - H * 2);
-    pts[2] := point(x2 + L, y2 - H);
-    pts[3] := point(x2, y2 + H);
-    Canvas.pen.Color := clBlack;
-    Canvas.Brush.Color := clDkGray;
-    Canvas.Polygon(pts);
-
-    Canvas.Brush.Color := clLtGray;
-    Canvas.pen.Color := clWhite;
-    pts[1] := point(x2 - L, y2 - H * 2);
-    pts[2] := point(x2 - L, y2 - H);
-    pts[3] := point(x2, y2 + H);
-    Canvas.Polygon(pts);
-
-    if (x < clen - 1) and (y > 0) and (FZValues[x + 1, y - 1] - z >= 0) then
+    if (x = 0) or (FZValues[x-1, y ] < z) then
       begin
-        d := FZValues[x + 1, y - 1];
-        s := trunc((d - z) * L / 8);
-        s2 := trunc((d - z + 1) * L / 8);
-        if s <= L then
-          begin
-            pts[0] := point(x2 + s - L, y2 - H * 2 + trunc(s * H / L * 2));
-            pts[3] := point(x2 + s2 - L, y2 - H * 1 + trunc(s2 * H / L * 2 + 0.5));
-            Canvas.pen.Color := clDkGray;
-            Canvas.Brush.Color := clDkGray;
-            Canvas.Polygon(pts);
-            Canvas.pen.Color := clBlack;
-            Canvas.Line(pts[1], pts[2]);
-            Canvas.Line(pts[2], pts[3]);
-            pts[0] := point(x2, y2);
-          end;
+        // PaintBox1.Canvas.pen.Color := PaintBox1.Canvas.Brush.Color;
+        pts[1] := point(x2 + L, y2 - H * 2);
+        pts[2] := point(x2 + L, y2 - H);
+        pts[3] := point(x2, y2 + H);
+        Canvas.pen.Color := clBlack;
+        Canvas.Brush.Color := clDkGray;
+        Canvas.Polygon(pts);
       end;
 
+    if (y = 0) or (FZValues[x , y-1] < z) then
+      begin
+        Canvas.Brush.Color := clLtGray;
+        Canvas.pen.Color := clWhite;
+        pts[1] := point(x2 - L, y2 - H * 2);
+        pts[2] := point(x2 - L, y2 - H);
+        pts[3] := point(x2, y2 + H);
+        Canvas.Polygon(pts);
+
+        if (x < clen - 1) and (y > 0) and (FZValues[x + 1, y - 1] - z >= 0) then
+          begin
+            d := FZValues[x + 1, y - 1];
+            s := trunc((d - z) * L / 8);
+            s2 := trunc((d - z + 1) * L / 8);
+            if s2 <= L then
+              begin
+                pts[0] := point(x2 + s - L, y2 - H * 2 + trunc(s * H / L * 2));
+                pts[3] := point(x2 + s2 - L, y2 - H * 1 + trunc(s2 * H / L * 2 + 0.5));
+                Canvas.pen.Color := clDkGray;
+                Canvas.Brush.Color := clDkGray;
+                Canvas.Polygon(pts);
+                Canvas.pen.Color := clBlack;
+                Canvas.Line(pts[1], pts[2]);
+                Canvas.Line(pts[2], pts[3]);
+                pts[0] := point(x2, y2);
+              end;
+          end;
+      end;
     // PaintBox1.Canvas.PolyLine(pts);
     // PaintBox1.Canvas.pen.Color := PaintBox1.Canvas.Brush.Color;
     if DrawHead then
@@ -328,7 +342,7 @@ begin
             s := trunc((FZValues[x + 1, y] - z) * L / 8);
             pts[1] := point(x2 + s, y2 - H * 4 + trunc(s * H / L * 2));
             pts[0] := point(x2 + s - L, y2 - H * 2 + trunc(s * H / L * 2));
-            Canvas.pen.Color := clDkGray;
+            Canvas.pen.Color := clltGray;
             Canvas.Brush.Color := clDkGray;
             Canvas.Polygon(pts);
           end;
@@ -360,19 +374,19 @@ var
 begin
     if Length(FZValues) < Clen then
         btnGenerateClick(Sender);
-  if PrintDialog1.Execute then
-    begin
-      printer.Orientation:=poLandscape;
-      printer.Title:='Laby #4 '+DateToStr(now());
-      Printer.BeginDoc;
-      for z := 0 to (Clen * 2 + 4) do
-          for x := Clen - 1 downto 0 do
-              for y := Clen - 1 downto 0 do
-                  if (FZValues[x, y] >= z) and ((x = 0) or (y = 0) or
-                      (z > baselevel(x, y) - 4)) then
-                      Drawbox(printer.Canvas, x, y, z, FZValues[x, y] = z);
-      Printer.EndDoc;
-    end;
+    if PrintDialog1.Execute then
+      begin
+        printer.Orientation := poLandscape;
+        printer.Title := 'Laby #4 ' + DateToStr(now());
+        Printer.BeginDoc;
+        for z := 0 to (Clen * 2 + 4) do
+            for x := Clen - 1 downto 0 do
+                for y := Clen - 1 downto 0 do
+                    if (FZValues[x, y] >= z) and
+                        ((x = 0) or (y = 0) or (z > baselevel(x, y) - 4)) then
+                        Drawbox(printer.Canvas, x, y, z, FZValues[x, y] = z);
+        Printer.EndDoc;
+      end;
 end;
 
 end.
